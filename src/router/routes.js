cbfb1bf37cd4567c55dc9303d1531d6d722be808
Xanadu-zxl <xanadu.zxl@gmail.com>
2020-04-17 16:29:51 +0800

    feat router

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,11 +9,31 @@ export default [
   {
     path: '/building',
     name: 'building',
-    component: () => import(/* webpackChunkName:'community' */'@/components/building')
+    component: () => import(/* webpackChunkName:'building' */'@/components/building')
   },
   {
     path: '/unit',
     name: 'unit',
-    component: () => import(/* webpackChunkName:'community' */'@/components/unit')
-  }
+    component: () => import(/* webpackChunkName:'unit' */'@/components/unit')
+  },
+  {
+    path: '/company',
+    name: 'company',
+    component: () => import(/* webpackChunkName:'company' */'@/components/company')
+  },
+  // {
+  //   path: '/pdf',
+  //   name: 'pdf',
+  //   component: () => import(/* webpackChunkName:'pdf' */'@/components/pdf')
+  // },
+  {
+    path: '/enterprise',
+    name: 'enterprise',
+    component: () => import(/* webpackChunkName:'enterprise' */'@/components/enterprise')
+  },
+    {
+    path: '*',
+    name: '404',
+    component: () => import(/* webpackChunkName:'404' */'@/components/404')
+  },
 ]
